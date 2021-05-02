0051609a771cf3ae1839651f01f9e7c234eba62c
Stanimir Stoychev <xstukixs@gmail.com>
2021-05-02 12:59:54 +0200

    Extend RomanNumerals test cases

--- src/RomanNumerals/__tests__/fromRoman.test.js
+++ src/RomanNumerals/__tests__/fromRoman.test.js
@@ -2,6 +2,16 @@ import fromRoman from '../fromRoman';
 import testCases from '../test-cases.json';
 
 describe('RomanNumerals (fromRoman)', () => {
+    it('should return undefined when provided with invalid input', () => {
+        expect(fromRoman()).toBeUndefined();
+        expect(fromRoman(1000)).toBeUndefined();
+        expect(fromRoman(true)).toBeUndefined();
+        expect(fromRoman(false)).toBeUndefined();
+        expect(fromRoman({})).toBeUndefined();
+        expect(fromRoman([])).toBeUndefined();
+        expect(fromRoman('')).toBeUndefined();
+    });
+
     it('should return expected values for pre-defined test cases', () => {
         Object.keys(testCases).forEach((number, index) => {
             expect(fromRoman(testCases[number])).toStrictEqual(index ? Number(number) : undefined);
--- src/RomanNumerals/__tests__/toRoman.test.js
+++ src/RomanNumerals/__tests__/toRoman.test.js
@@ -2,6 +2,21 @@ import toRoman from '../toRoman';
 import testCases from '../test-cases.json';
 
 describe('RomanNumerals (toRoman)', () => {
+    it('should return undefined when provided with invalid input', () => {
+        expect(toRoman()).toBeUndefined();
+        expect(toRoman(true)).toBeUndefined();
+        expect(toRoman(false)).toBeUndefined();
+        expect(toRoman({})).toBeUndefined();
+        expect(toRoman([])).toBeUndefined();
+        expect(toRoman('')).toBeUndefined();
+        expect(toRoman('{}')).toBeUndefined();
+    });
+
+    it('should throw an exception when out of bounds', () => {
+        expect(() => toRoman(4000)).toThrowError('"4000" is out of bounds! Roman numerals go up to 3999!');
+        expect(() => toRoman(10000)).toThrowError('"10000" is out of bounds! Roman numerals go up to 3999!');
+    });
+
     it('should return expected values for pre-defined test cases', () => {
         Object.keys(testCases).forEach((number, index) => {
             expect(toRoman(Number(number))).toStrictEqual(testCases[number]);
